bb74821cdd352f92ddbaa327e5fd8ea8294b42b7
Damitha Raveendra <deamonsarea52@gmail.com>
2023-08-16 16:58:08 +0530

    Updates to Video View
    # Icons rearranged

--- InstaClone/Core/Shorts/VideoShortsView.swift
+++ InstaClone/Core/Shorts/VideoShortsView.swift
@@ -9,28 +9,39 @@ import SwiftUI
 
 struct VideoShortsView: View {
     var body: some View {
-        VStack (alignment: .trailing, spacing: 20) {
-            Button {
+        HStack {
+            Spacer()
+            VStack (spacing: 20) {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "heart")
+                        .resizable()
+                        .foregroundColor(.white)
+                        .frame(width: 30, height: 30)
+                }
                 
-            } label: {
-                Image(systemName: "heart")
-                    .imageScale(.large)
-            }
-            
-            Button {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "message")
+                        .resizable()
+                        .foregroundColor(.white)
+                        .frame(width: 30, height: 30)
+                }
                 
-            } label: {
-                Image(systemName: "message")
-                    .imageScale(.large)
-            }
-            
-            Button {
-                
-            } label: {
-                Image(systemName: "paperplane")
-                    .imageScale(.large)
+                Button {
+                    
+                } label: {
+                    Image(systemName: "paperplane")
+                        .resizable()
+                        .foregroundColor(.white)
+                        .frame(width: 30, height: 30)
+                }
             }
         }
+        .padding(20)
+        .background(.black)
     }
 }
 
